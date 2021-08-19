1d4d4ebc8d74e4ba7687d1b1e1fe54e296ca40ea
Rosa-MarciaOlive <rosa.marciaolive@gmail.com>
2021-08-19 17:30:30 +0000

    Imprementação.

--- BalancoTrimestral.java
+++ BalancoTrimestral.java
@@ -1,3 +1,14 @@
 public class BalancoTrimestral{
-    
+    public static void main (String args[]){
+        byte idade=36;
+        int habitantes=22000;
+        double salario=1100.0;
+        String nome= "Rosa márcia";
+        boolean semaforoAberto=true;
+        long celular=240000L;
+        char letra='R';
+        //Exibe utilizando classes a cima.
+        System.out.println("Sou a"+nome+",tenho"+idade+"de idade.");
+        System.out.println("Meu sálario é: "+salario+".Fim");
+            }
 }
