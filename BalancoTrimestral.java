2ed91626489c8b40ee39b8670f2dccbe256898e5
Rosa-MarciaOlive <rosa.marciaolive@gmail.com>
2021-08-19 17:55:24 +0000

    Finalizado

--- BalancoTrimestral.java
+++ BalancoTrimestral.java
@@ -1,14 +1,13 @@
 public class BalancoTrimestral{
     public static void main (String args[]){
         byte idade=36;
-        int habitantes=22000;
-        double salario=1100.0;
-        String nome= "Rosa márcia";
-        boolean semaforoAberto=true;
-        long celular=240000L;
-        char letra='R';
+        int gastosJaneiro=15000;
+        int gastosFevereiro=22000;
+        int gastosMarco=17888;
+        int gastosTrimestral=gastosJaneiro+gastosFevereiro+gastosMarco;
+        int gastoMensal=gastosTrimestral/3;
         //Exibe utilizando classes a cima.
-        System.out.println("Sou a"+nome+",tenho"+idade+"de idade.");
-        System.out.println("Meu sálario é: "+salario+".Fim");
+        System.out.println("O gasto trimestral foi de: "+gastosTrimestral);
+        System.out.println("O gasto mensal foi de: "+gastoMensal);
             }
 }
